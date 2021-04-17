a3ed2210d577822beffbcab52cb1f75247779146
IWPTeam <68476525+IWPTeam@users.noreply.github.com>
2021-04-17 17:13:42 +0300

    add design and some methods in insert data activity

--- app/src/main/java/com/aboba/room/InsertDataActivity.kt
+++ app/src/main/java/com/aboba/room/InsertDataActivity.kt
@@ -2,7 +2,10 @@ package com.aboba.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import coil.load
 import com.aboba.room.databinding.ActivityInsertDataBinding
+import com.google.android.material.snackbar.Snackbar
 
 class InsertDataActivity : AppCompatActivity() {
     lateinit var binding: ActivityInsertDataBinding
@@ -10,5 +13,18 @@ class InsertDataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityInsertDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun push(view: View) {
+        Snackbar.make(view,
+            binding.etName.text.toString(),
+            Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun preload(view: View) {
+        binding.preloadContainer.load(binding.etImageUrl.text.toString()){
+            placeholder(R.drawable.ic_baseline_timer_128)
+            error(R.drawable.ic_baseline_error_outline_24)
+        }
     }
 }
